2e4fb15db4c22f16fdeb1417feff25d80fea1b15
RayCode03 <raymonddrg03@gmail.com>
2023-04-11 21:27:45 -0400

    Add Books

---
+++ controllers/BooksController.js
@@ -0,0 +1,11 @@
+const User = require("../models/User");
+
+
+exports.GetBooks = (request, response, next) => {
+  //verificar si el usuário está logueado
+
+
+  /*  return response.status(200).render("books", {
+    pageTitle: "Books",
+  }); */
+};
