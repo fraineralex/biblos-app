43af2478e71811ec85a7476a7efe0f8a997554cd
Frainer Encarnación <fraineralex2001@gmail.com>
2023-04-12 23:28:27 -0400

    [merge] merge between remote and local repository

---
+++ tests/user.test.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+const { api, getUsers } = require('./helpers')
+const sequelize = require('sequelize')
+const { server } = require('../index')
+
+describe('creating a new user', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('1234', 10)
+    const user = new User({ username: 'miduroot', passwordHash })
+
+    await user.save()
+  })
+
+  test('works as expected creating a fresh username', async () => {
+    const usersAtStart = await getUsers()
+
+    const newUser = {
+      username: 'fulano08',
+      name: 'Fulano',
+      password: 'Sanchez'
+    }
+
+    await api
+      .post('/signup')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await getUsers()
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
